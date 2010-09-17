5d56e41424fa839e252d1c12b82dec4d54f9fb53
Vitor Pellegrino <vitorp@gmail.com>
2010-09-17 11:07:56 -0300

    Import build-essential version 0.7.0

--- roles/repository.rb
+++ roles/repository.rb
@@ -1,6 +1,6 @@
 name "Repository" 
 description "Repository role for a machine that holds organization source code"
-run_list "recipe[getting-started]" , "recipe[zlib]", "recipe[apache2]"  
+run_list "recipe[getting-started]" , "recipe[zlib]", "recipe[apache2]" , "recipe[git]" 
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
