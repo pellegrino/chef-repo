c1a9b57dc09acca23a76a4d72fcffe074137bd9d
Vitor Pellegrino <vitorp@gmail.com>
2010-09-17 08:39:39 -0300

    Import git version 0.9.0

--- roles/repository.rb
+++ roles/repository.rb
@@ -1,6 +1,6 @@
 name "Repository" 
 description "Repository role for a machine that holds organization source code"
-run_list "recipe[getting-started]" , "recipe[zlib]" 
+run_list "recipe[getting-started]" , "recipe[zlib]", "recipe[apache2]"  
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
