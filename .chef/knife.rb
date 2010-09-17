5f71e982c5545354fe25ea5c0e0a133e717d75ff
Vitor Pellegrino <vitorp@gmail.com>
2010-09-17 07:42:19 -0300

    added chef credentials

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "pellegrino"
+client_key               "#{current_dir}/pellegrino.pem"
+validation_client_name   "quartieri-validator"
+validation_key           "#{current_dir}/quartieri-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/quartieri"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright "quartieri.com.br"
+cookbook_email     "vitor@quartieri.com.br"
+cookbook_license   "apachev2"
+
