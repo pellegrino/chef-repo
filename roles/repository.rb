45b04c990291258c8ed576c5d56e49274a3578af
Vitor Pellegrino <vitorp@gmail.com>
2010-09-17 07:51:43 -0300

    Added the repository role

---
+++ roles/repository.rb
@@ -0,0 +1,8 @@
+name "Repository" 
+description "Repository role for a machine that holds organization source code"
+run_list('getting_started')
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+# Attributes applied no matter what the node has set already.
+#override_attributes()
+
