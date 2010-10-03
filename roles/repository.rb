7ced8df61c8219877150416aab17f9b61f9f42df
Vitor Pellegrino <vitorp@gmail.com>
2010-10-02 22:52:32 -0300

    Import database version 0.6.0

--- roles/repository.rb
+++ roles/repository.rb
@@ -1,6 +1,21 @@
 name "Repository" 
 description "Repository role for a machine that holds organization source code"
 run_list "recipe[sphinx]", "recipe[gitorious]"
+override_attributes :apps => {
+  :my_app => {
+    :production => { 
+      :run_migrations => true,
+      :force => false    
+    }, 
+    :staging => { 
+      :run_migrations => true , 
+      :force => true 
+    } 
+  }
+
+}
+
+
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
